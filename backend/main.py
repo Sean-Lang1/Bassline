9c41a20b68b307d040dde850a03d7057152769c8
Sean Lang <112538164+Sean-Lang1@users.noreply.github.com>
2026-07-13 00:56:51 -0400

    test

--- backend/main.py
+++ backend/main.py
@@ -44,70 +44,12 @@ def get_artist(name: str):
 
 @app.get("/relations/{name}")
 def get_relations(name: str):
+    data = build_artist_relations(name)
 
-    conn = get_connection()
-    cur = conn.cursor()
+    if not data:
+        raise HTTPException(status_code=404, detail="Artist not found")
 
-    try:
-
-        cur.execute(
-            "SELECT * FROM artists WHERE name ILIKE %s",
-            (name,)
-        )
-
-        artist = cur.fetchone()
-
-        if not artist:
-
-            data = build_artist_relations(name)
-
-            if not data:
-                raise HTTPException(
-                    status_code=404,
-                    detail="Artist not found"
-                )
-
-            cur.execute(
-                "SELECT * FROM artists WHERE spotify_id = %s",
-                (data["artist"]["id"],)
-            )
-
-            artist = cur.fetchone()
-
-        cur.execute(
-            """
-            SELECT
-                a.*,
-                l.relationship AS rel_type
-            FROM artist_links l
-            JOIN artists a
-                ON a.spotify_id = l.target_id
-            WHERE l.source_id = %s
-            ORDER BY
-            CASE l.relationship
-                WHEN 'member of band' THEN 1
-                WHEN 'discovered' THEN 2
-                WHEN 'discovered by' THEN 2
-                WHEN 'sibling' THEN 3
-                WHEN 'married' THEN 3
-                WHEN 'collaboration' THEN 4
-                ELSE 5
-            END,
-            a.listeners DESC
-            """,
-            (artist["spotify_id"],)
-        )
-
-        related = cur.fetchall()
-
-    finally:
-
-        conn.close()
-
-    return {
-        "artist": dict(artist),
-        "related": [dict(r) for r in related]
-    }
+    return data
 
 
 @app.get("/favicon.ico", include_in_schema=False)
